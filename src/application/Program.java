21b7360804f26599d2649f2d7db6773dda8f6ccc
Bruno H. Homem <brunohhomem@gmail.com>
2023-11-24 15:59:21 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -46,7 +46,6 @@ public class Program {
 				System.out.println("Press enter to continue...");
 
 				sc.nextLine();
-
 			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				System.out.println("Press enter to continue...");
